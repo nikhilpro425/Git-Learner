067a079eec5a09ddff5e3608c77e287c6b9af483
Nikhil Saini <nick@pop-os.localdomain>
2021-05-27 22:27:33 +0530

    Added resepctable and readable content in files!

--- forloops.py
+++ forloops.py
@@ -1,6 +1,2 @@
 for i in range(0, 10):
     print("Brother!, Brother Here!")
-
-print("done with for loops")
-print("aur btao chacha")
-
--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,2 @@
 print("Hello World")
-
-print("chacha bidhayak h hamare")
-
 print("New change in dev branch")
